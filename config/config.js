09c06d64d30d91804f5f2803d870a26c8848b068
Jahnavi Bhagirath Patel <62233606+jan-code26@users.noreply.github.com>
2022-04-17 23:00:18 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -6,13 +6,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDOHGtdz1v3QyCpYluXEI1vZfJ4XOsoIHc",
-  authDomain: "doctalks-80632.firebaseapp.com",
-  projectId: "doctalks-80632",
-  storageBucket: "doctalks-80632.appspot.com",
-  messagingSenderId: "275250205223",
-  appId: "1:275250205223:web:16ba4cc33029c9f9e4f402",
-  measurementId: "G-QGR1Z9BVQZ"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
